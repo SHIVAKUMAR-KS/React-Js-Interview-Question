713b3a24ea6bcdb4a698ac5ce57070e6c5b70c27
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-06-17 20:45:00 +0530

    added newone

---
+++ Moving_Div/src/components/Movable.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Movable = () => {
+  return (
+    <div>
+      <button>Left</button>
+      <button>Right</button>
+      <button>Up</button>
+      <button>Down</button>
+    </div>
+  );
+};
+
+export default Movable;
+Movable;
