c42962c04ad4f5fb48daa82cced44832848bfce0
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-06-19 21:28:08 +0530

    donewith movable div project

--- Moving_Div/src/components/Movable.jsx
+++ Moving_Div/src/components/Movable.jsx
@@ -1,27 +1,53 @@
-import React from "react";
+import { useState, useRef } from "react";
 
-const Movable = () => {
-  return (
-    <div>
-      <div>
-        <button>Up</button>
-      </div>
-      <div>
-        <button>Left</button>
-      </div>
-      <div>
-        <button>Up</button>
-      </div>
-      <div>
-        <button>Down</button>
-      </div>
-     
-      <div>
-        <div className="block"></div>
-      </div>
-    </div>
-  );
-};
+const Moveable = () => {
+    const [moving, setMoving] = useState({ x: 0, y: 0 })
+    const blockRef = useRef();
 
-export default Movable;
-Movable;
+    const moveBlock = (dir) => {
+
+        const boundries = {
+            up: moving.y <= 0,
+            left: moving.x <=0,
+            right: moving.x >= blockRef?.current.offsetWidth - 200,
+            down: moving.y >= blockRef?.current.clientHeight - 200
+        }
+
+        if (boundries[dir]) return;
+
+        const updates = {
+            up: { y: moving.y - 10 },
+            down: { y: moving.y + 10 },
+            left: { x: moving.x - 10 },
+            right: { x: moving.x + 10 }
+        }
+
+        setMoving(prevState => ({ ...prevState, ...updates[dir] }));
+    }
+
+    return (
+        <div ref={blockRef}>
+            <div>
+                <button className="horizontal" onClick={() => moveBlock("up")}>Up</button>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                <button  className="vertical" onClick={() => moveBlock("left")}>Left</button>
+                <div className="wrapper">
+                    <div 
+                        className="block"
+                        style={{
+                            position: 'absolute',
+                            left: moving.x,
+                            top: moving.y
+                        }}></div>
+                </div>
+                <button className="vertical" onClick={() => moveBlock("right")}>Right</button>
+            </div>
+            <div>
+                <button className="horizontal" onClick={() => moveBlock("down")}>Down</button>
+            </div>        
+        </div>
+    )
+}
+
+export default Moveable;
